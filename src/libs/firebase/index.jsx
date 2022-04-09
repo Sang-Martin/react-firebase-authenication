3ab9cdc45f201ffe170f3e674a3a1c767296e261
Sang-Martin <59576275+Sang-Martin@users.noreply.github.com>
2022-04-09 02:48:17 -0600

    Update index.jsx

--- src/libs/firebase/index.jsx
+++ src/libs/firebase/index.jsx
@@ -6,14 +6,22 @@ import {getAuth} from "firebase/auth"
 // Set Up the Firebase Config with .env the Create React App Way.
 // This will have to be added to netlify.
 const firebaseConfig = {
-  apiKey:process.env.API_KEY,
-  authDomain:process.env.AUTH_DOMAIN,
-  databaseURL:process.env.DATABASE_URL,
-  projectId:process.env.PROJECT_ID,
-  storageBucket:process.env.STORAGE_BUCKET,
-  messagingSenderId:process.env.MESSAIGN_SENDER_ID,
-  appId:process.env.APP_ID,
-  measurementId:process.env.MEASUREMENT_ID
+    apiKey: "AIzaSyD5-KrhjHd1mo61E1oOPGPy6jwdBFrPSdc",
+  authDomain: "realtime-database-345f2.firebaseapp.com",
+  databaseURL: "https://realtime-database-345f2-default-rtdb.firebaseio.com",
+  projectId: "realtime-database-345f2",
+  storageBucket: "realtime-database-345f2.appspot.com",
+  messagingSenderId: "736902782319",
+  appId: "1:736902782319:web:46849fad415674ac99dc9a",
+  measurementId: "G-1566DTHFVR"
+//   apiKey:process.env.API_KEY,
+//   authDomain:process.env.AUTH_DOMAIN,
+//   databaseURL:process.env.DATABASE_URL,
+//   projectId:process.env.PROJECT_ID,
+//   storageBucket:process.env.STORAGE_BUCKET,
+//   messagingSenderId:process.env.MESSAIGN_SENDER_ID,
+//   appId:process.env.APP_ID,
+//   measurementId:process.env.MEASUREMENT_ID
 };
 
 // Initialize Firebase App and associated services
